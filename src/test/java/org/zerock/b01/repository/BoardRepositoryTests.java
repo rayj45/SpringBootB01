33291871be19de9056c9ca1ba4d511ff0099d750
rayj45 <rayj45@naver.com>
2024-09-19 22:21:51 +0900

    240919 commit

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -60,5 +65,25 @@ public class BoardRepositoryTests {
 
         boardRepository.deleteById(bno);
     }
+
+    @Test
+    public void testPaging(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("bno").descending()); //Pageable , PageRequest.of - 페이징 처리
+
+        Page<Board> result = boardRepository.findAll(pageable); //bno컬럼으로 내림차순 정렬하여 10개 데이터인 1번째 페이지를 조회
+        //findAll리턴 타입으로 나오는 Page<T>타입은 페이징 처리에 필요한 여러 정보들 포함(다음,이전페이지 존재여부, 전체데이터 개수 등)
+
+        log.info("total count : " + result.getTotalElements());
+        log.info("total pages : " + result.getTotalPages());
+        log.info("page number : " + result.getNumber());
+        log.info("page size : " + result.getSize());
+
+        log.info("---------------------------------");
+        List<Board> todoList = result.getContent(); //페이징 처리 객체의 내용을 담음
+
+        todoList.forEach(board -> log.info(board));
+    }
+
+
 
 }
