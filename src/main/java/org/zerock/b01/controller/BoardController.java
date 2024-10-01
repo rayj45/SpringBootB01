3b2bee5cbbe5c29b2aa9322a866a4505a1ed8fc7
rayj45 <rayj45@naver.com>
2024-10-01 22:34:12 +0900

    241001 commit

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -4,11 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -28,4 +34,31 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO); //PageResponseDTO와 PageRequestDTO객체가 화면에 전달됨(model을 통해서)
         //responseDTO라는 이름으로 위에서 정의한 responseDTO객체가 model에 저장되어 view에서 사용할 수 있음
     } //list
+
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        log.info("board POST register...");
+
+        //에러 발생 시 redirection처리
+        if (bindingResult.hasErrors()){
+            log.info("has errors...");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            //post처리 시 받은 매개변수에 @Valid를 통해 검증하고, bindingResult로 에러를 확인하는 부분 필요..!
+
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
 }
